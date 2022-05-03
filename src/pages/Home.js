1931e3bb25824fb11a84ab20362dff1c25c2a4e3
Kaylah Gore <5260333-kaylahgore@users.noreply.gitlab.com>
2022-05-02 21:12:34 -0500

    add front end w/ connect wallet & background image

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,32 @@
 import React from "react";
 import "./Home.css";
 import { Link } from "react-router-dom";
-
+import bg from "../images/frontpagebg2.png";
+import logo from "../images/airbnbRed.png";
+import {ConnectButton} from "web3uikit";
 const Home = () => {
 
   return (
     <>
-      <Link to="/rentals"> Rentals </Link>
+      <div className="container" style={{backgroundImage: `url({$bg})`}}>
+        <div className="containerGradient">
+          <div className="topBanner">
+            <div>
+              <img className="logo" src="{logo}" alt="logo"></img>
+            </div>
+            <div className="tabs">
+              <div className="selected"> Places to stay</div>
+              <div>Experiences</div>
+              <div>Online Experiences</div>
+            </div>
+            <div className="lrContainers">
+            <ConnectButton />
+
+            </div>
+          </div>
+
+        </div>
+      </div>
     </>
   );
 };
